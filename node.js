dbb0e2ee363cfbe2a0f10e32bbe5f253e05bbec8
Vxre <rox.gaming67@gmail.com>
2020-04-14 11:30:24 -0400

    azeae

--- node.js
+++ node.js
@@ -19,26 +19,69 @@ console​ .log(​ "La couleur est jaune"​ );
 console​ .log(​ `La couleur était: ​ ${couleur}​ ` ​ );
 }*/
 
-let tablalColor = ["bleu","rouge","jaune","vert","blue"];
-for (let index = 0; index >= tablalColor.length;index++){
+
+
+
+/*let tablalColor = ["bleu","rouge","jaune","vert","blue","roux"];
+for (let index = 0; index < tablalColor.length;index++){
    switch (tablalColor[index]) {
        case "bleu":
-            console.log(`La couleur à l'index ${index} est : [0]`);       
+            console.log(`La couleur à l'index ${index} est : bleu`);       
            break;
         case "rouge":
-            console.log(`La couleur à l'index ${index} est : [1]`);
+            console.log(`La couleur à l'index ${index} est : rouge`);
             break;
         case "jaune":
-            console.log(`La couleur à l'index ${index} est : [2]`);
+            console.log(`La couleur à l'index ${index} est : jaune`);
             break;
-        case "jaune":
-            console.log(`La couleur à l'index ${index} est : [3]`);
+        case "vert":
+            console.log(`La couleur à l'index ${index} est : vert`);
             break;
    
        default:
-           console.log(`La couleur [tablalColor] n'était pas attendue`);
-            console.log("test")
+           console.log(`La couleur ${[tablalColor[index]]} n'était pas attendue`);
            break;
            
    }
+}*/
+
+
+
+//Fonction
+/*function add(a,b){
+    let resultat = (a + b);
+    return(resultat);
 }
+console.log(add(1,1));*/
+
+/*const​ couleur = array => {
+    let​ res1 = ​ ""​ ;
+    for​ (​ let​ i = ​ 0 ​ ; i < array.length; i++) {
+        res1 += ​ `Il y a la couleur ​ ${array[i]}​ à l'index ​ ${i}​ !`​ ;
+        if​ (i !== array.length - ​ 1 ​ ) {​ 
+            res1 += ​ '\n'​ ;
+        }
+    }
+    return​ (res1);
+    }
+console​ .log(couleur([​ "bleu"​ , ​ "rouge"​ , ​ "jaune"​ , ​ "vert"​ , "violet"​ , ​ "blanc"​ ]));
+*/
+
+const player = {
+    nom: "Jean",
+    prénom: "Jass",
+    sexe: "Homme",
+    tenue: "En Claquette Chaussette",
+    listSac:["un stylo", "une choucroute"],
+
+    description() {
+        return (`Je suis un ${this.sexe}, je m'appelle ${this.nom} ${this.prénom}, aujourd'hui je suis habillé ${this.tenue}, je porte un sac à dos qui contient ${this.listSac}`)
+    }
+};
+console.log(player.description());
+
+
+
+
+
+
