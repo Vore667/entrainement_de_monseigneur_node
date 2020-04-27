bd1ce591719d08c6856cc7bda1bda2fbd431690c
Vxre <rox.gaming67@gmail.com>
2020-04-27 08:38:16 -0400

    sdf

--- node.js
+++ node.js
@@ -1,87 +1,23 @@
-/*let age = 18;
-if (age >= 18){
-    console.log("Vous êtes majeurs");
-}
-else{
-    console.log("Vous êtes mineur");
-}
-
-let​ couleur = ​bleu ​;
-if​ (couleur === ​ "bleu"​ ) {
-console​ .log(​ "La couleur est bleu"​ );
-} ​ else​ ​ if​ (couleur === ​ "rouge"​ ) {
-console​ .log(​ "La couleur est rouge"​ );
-} ​ else​ ​ if​ (couleur === ​ "vert"​ ) {
-console​ .log(​ "La couleur est vert"​ );
-} ​ else​ ​ if​ (couleur === ​ "jaune"​ ) {
-console​ .log(​ "La couleur est jaune"​ );
-} ​ else​ {
-console​ .log(​ `La couleur était: ​ ${couleur}​ ` ​ );
-}*/
-
-
-
-
-/*let tablalColor = ["bleu","rouge","jaune","vert","blue","roux"];
-for (let index = 0; index < tablalColor.length;index++){
-   switch (tablalColor[index]) {
-       case "bleu":
-            console.log(`La couleur à l'index ${index} est : bleu`);       
-           break;
-        case "rouge":
-            console.log(`La couleur à l'index ${index} est : rouge`);
-            break;
-        case "jaune":
-            console.log(`La couleur à l'index ${index} est : jaune`);
-            break;
-        case "vert":
-            console.log(`La couleur à l'index ${index} est : vert`);
-            break;
-   
-       default:
-           console.log(`La couleur ${[tablalColor[index]]} n'était pas attendue`);
-           break;
-           
-   }
-}*/
-
-
-
-//Fonction
-/*function add(a,b){
-    let resultat = (a + b);
-    return(resultat);
-}
-console.log(add(1,1));*/
-
-/*const​ couleur = array => {
-    let​ res1 = ​ ""​ ;
-    for​ (​ let​ i = ​ 0 ​ ; i < array.length; i++) {
-        res1 += ​ `Il y a la couleur ​ ${array[i]}​ à l'index ​ ${i}​ !`​ ;
-        if​ (i !== array.length - ​ 1 ​ ) {​ 
-            res1 += ​ '\n'​ ;
-        }
+/*let entreeStandard = process.stdin;
+entreeStandard.setEncoding("utf-8");
+console.log("Dites quelque chose.\nDites \'exit\' pour quitter.");
+entreeStandard.on("data", function (data) {
+    if (data === "exit\n" || data === "exit\r\n") {
+    //on vérifie si l'utilisateur a entré le mot exit (+ '\n' car quand on appuis sur entrer ça l'ajoute automatiquement ou '\r\n' même caractère mais le second est est générer par Windows)
+    console.log("Récupération des information complète,fermeture du programme.");
+    process.exit();
+    } else if (data !== "\r\n" && data !== "\n") {
+    console.log('Vous avez dit: ' + data);
     }
-    return​ (res1);
+    });*/
+
+let allumette = 13;
+let main = process.stdin;
+main.setEncoding("utf-8");
+console.log("Bienvenue dans le jeu");
+main.on("data", function (data){
+    if (data === "exit\n" || data === "exit \r\n"){
+        console.log("Récupération des informations complète, fermeture du programme. ")
     }
-console​ .log(couleur([​ "bleu"​ , ​ "rouge"​ , ​ "jaune"​ , ​ "vert"​ , "violet"​ , ​ "blanc"​ ]));
-*/
-
-const player = {
-    nom: "Jean",
-    prénom: "Jass",
-    sexe: "Homme",
-    tenue: "En Claquette Chaussette",
-    listSac:["un stylo", "une choucroute"],
-
-    description() {
-        return (`Je suis un ${this.sexe}, je m'appelle ${this.nom} ${this.prénom}, aujourd'hui je suis habillé ${this.tenue}, je porte un sac à dos qui contient : \n` ${this.listSac.forEach(elemet => console.log(element + `\n`)});
-    }
-};
-console.log(player.description());
-
-
-
-
-
+});
 
